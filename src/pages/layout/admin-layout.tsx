6823ad6724574e0bebb644e774efac87df97ceac
awale-roman <romanawale7@gmail.com>
2026-01-26 21:44:24 +0545

    admin layout sider

--- src/pages/layout/admin-layout.tsx
+++ src/pages/layout/admin-layout.tsx
@@ -1,10 +1,17 @@
-import { Button, Layout, Menu } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
 import {
+  AppstoreAddOutlined,
   DashboardOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
+  ProductOutlined,
+  StarOutlined,
+  TagOutlined,
+  TeamOutlined,
+  TruckOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
+import { Button, Layout, Menu } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
 
 import Sider from "antd/es/layout/Sider";
 import logo from "../../assets/images/logo.png";
@@ -14,21 +21,23 @@ const AdminLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <>
-      {/* <div className="h-screen w-full flex ">
-        <div className="slider h-full w-1/3 bg-blue-400">slider</div>
-        <div className="dasboard-right-panel h-full flex flex-col grow">
-          <div className="header p-[20px] bg-red-400 ">header</div>
-          <div className="content p-[20px]  bg-red-100 grow">content</div>
-          <div className="footer p-[20px]  bg-red-400">footer</div>
-        </div>
-      </div> */}
       <Layout>
-        <Sider collapsible collapsed={collapsed}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
+          className="h-screen"
+        >
           <div>
-            <div>
-              <img src={logo} alt="logo" />
+            <div className="p-4 flex justify-center">
+              <div className="w-[100px] rounded-full overflow-hidden">
+                <img src={logo} alt="logo" />
+              </div>
             </div>
             <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={["1"]}
               items={[
                 {
                   key: "1",
@@ -37,32 +46,32 @@ const AdminLayout = () => {
                 },
                 {
                   key: "2",
-                  icon: <DashboardOutlined />,
+                  icon: <StarOutlined />,
                   label: "Banners",
                 },
                 {
                   key: "3",
-                  icon: <DashboardOutlined />,
+                  icon: <TagOutlined />,
                   label: "Brands",
                 },
                 {
                   key: "4",
-                  icon: <DashboardOutlined />,
+                  icon: <AppstoreAddOutlined />,
                   label: "Categories",
                 },
                 {
                   key: "5",
-                  icon: <DashboardOutlined />,
+                  icon: <ProductOutlined />,
                   label: "Products",
                 },
                 {
                   key: "6",
-                  icon: <DashboardOutlined />,
+                  icon: <TruckOutlined />,
                   label: "Orders",
                 },
                 {
                   key: "7",
-                  icon: <DashboardOutlined />,
+                  icon: <TeamOutlined />,
                   label: "Users",
                 },
               ]}
@@ -70,14 +79,19 @@ const AdminLayout = () => {
           </div>
         </Sider>
         <Layout>
-          <Header>
+          <Header className="flex justify-between items-center">
             <Button
               icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
               onClick={() => {
                 setCollapsed(!collapsed);
               }}
               type="text"
+              style={{
+                fontSize: "16px",
+                color: "white",
+              }}
             ></Button>
+            <Button icon={<UserOutlined />}></Button>
           </Header>
           <Content>content</Content>
         </Layout>
